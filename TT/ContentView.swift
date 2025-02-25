5e1179c053cb3d3b2c3a737fd4fb7fdd98bfb181
raf <rafvitale@chainml.net>
2025-02-25 16:49:51 -0500

    Initial Commit

---
+++ TT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TT
+//
+//  Created by Raf V. on 25/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TT/TTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TTApp.swift
+//  TT
+//
+//  Created by Raf V. on 25/02/25.
+//
+
+import SwiftUI
+
+@main
+struct TTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
